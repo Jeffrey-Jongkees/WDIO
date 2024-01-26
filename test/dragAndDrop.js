bd1f9783c5026adaff1d8ca66fd805071c4a5e16
Jeffrey Jongkees <jeffreyjongkees@bsure-digital.nl>
2024-01-26 15:11:12 +0100

    added testFile.txt

--- test/dragAndDrop.js
+++ test/dragAndDrop.js
@@ -32,4 +32,80 @@ describe('Drag and Drop', () => {
         await browser.pause(2000);
     
     }); 
+
+    it('Handling windows', async () => {
+        await browser.url('https://www.webdriveruniversity.com/'); 
+        await browser.newWindow('https://www.automationteststore.com/');
+
+        let currentWindow_Title = await browser.getTitle();
+        console.log(`>>Current Window Title: ${currentWindow_Title}`);
+        await expect(browser).toHaveUrlContaining('automationteststore');
+        
+
+        await browser.switchWindow('webdriveruniversity.com/')
+        let parentWindow_Title = await browser.getTitle();
+        console.log(`>>Current Window Title: ${parentWindow_Title}`);
+        await expect(browser).toHaveUrlContaining('webdriveruniversity');
+
+
+        await $('[id="contact-us"]').click() 
+        await browser.switchWindow('automationteststore')
+        await browser.closeWindow();
+
+        await browser.switchWindow('contactus')
+        await browser.closeWindow();
+
+        await browser.switchWindow('webdriveruni')
+        console.log(await browser.getTitle());
+    });
+
+    it('Iframes', async () => {
+        await browser.url('IFrame/index.html')
+        const iframe = await $('[id="frame"]')
+        await browser.switchToFrame(iframe)
+        await $('[href="products.html"]').click()
+        await browser.switchToParentFrame();
+    });
+
+    it('Alerts', async () => {
+        await browser.url('Popup-Alerts/index.html');
+        await $('[id="button1"]').click();
+        await browser.acceptAlert();
+
+        await $('[id="button4"]').click();
+        const alertText = await browser.getAlertText();
+        expect(alertText).toEqual('Press a button!');
+
+        await browser.acceptAlert();//uses the positive route. Pressing OK for this instance
+        await expect($('[id="confirm-alert-text"]')).toHaveText('You pressed OK!');
+        await browser.pause(2000);
+
+        await $('[id="button4"]').click();
+        await browser.dismissAlert();
+        await expect($('[id="confirm-alert-text"]')).toHaveText('You pressed Cancel!');
+
+    });
+
+    it('File upload', async () => {
+        await browser.url('File-Upload/index.html');
+        await $('[id="myFile"]').addValue(`${process.cwd()}\\test\\uploadTestFile.txt`)
+        await $('[id="submit-button"]').click()
+
+        const alertText = await browser.getAlertText();
+        expect(alertText).toEqual('Your file has now been uploaded!');
+
+    });
+
+
+    it('JS execute - hidden elements', async () => {
+        await browser.url('Hidden-Elements/index.html');
+        await browser.execute(()=> {
+            return document.getElementById('not-displayed').setAttribute("id", "");
+        })
+        
+        await browser.execute(()=> {
+            return document.body.style.backgroundColor = "tomato";
+        })
+
+    });
 });
